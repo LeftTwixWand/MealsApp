77c36d422273a6ded0d084c49ef5c1ac13dcec22
Vladyslav <lefttwixwand@gmail.com>
2021-07-10 23:01:41 +0300

    Added navigation to the details page

---
+++ lib/category_meals_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class CategoryMealsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: Text('The Recipes'),
+        ),
+        body: Text('This is Recipes Fro The Category!'),
+      );
+}
